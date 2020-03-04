4290392e394d48670f477ac26e543e00cebe831d
Ying-Li Shih <norelle@Nor.local>
2020-03-05 00:42:48 +0800

    added redux, and implemented userReducer and userActions to header and app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,47 +9,36 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   //Set the subscription to null by default
   unsubscribeFromAuth = null
 
   componentDidMount() {
-      //Using the onAuthStateChanged method from firebase auth to keep track of the authentication status (open subscription)
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-        if (userAuth) {
-          //When user login, create a user object in firestore
-          const userRef = await createUserProfileDocument(userAuth);
+    const { setCurrentUser } = this.props;
 
-          /**
-          The onSnapshot method will always check if our database has updated at that reference with any new data (i.e. see if the snapshot has changed)
-          and send us a snapshot object (the data that we stored in firestore)
-          **/
-          userRef.onSnapshot(snapShot => {
-            this.setState({
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data()
-              }
-            })
+    //Using the onAuthStateChanged method from firebase auth to keep track of the authentication status (open subscription)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        //When user login, create a user object in firestore
+        const userRef = await createUserProfileDocument(userAuth);
 
-            console.log(this.state);
+        /**
+        The onSnapshot method will always check if our database has updated at that reference with any new data (i.e. see if the snapshot has changed)
+        and send us a snapshot object (the data that we stored in firestore)
+        **/
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+              id: snapShot.id,
+              ...snapShot.data()
           })
-        } else {
-          //User logout
-          this.setState({
-            currentUser: userAuth
-          })
-        }
-      });
+        })
+      } else {
+        //User logout
+        setCurrentUser(userAuth)
+      }
+    });
   }
 
   //Close subscription
@@ -59,7 +49,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -70,4 +60,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import { connect } from 'react-redux';
+
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -27,4 +29,8 @@ const Header = ({ currentUser }) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
