9582fb4a5adf5bc55b0c92fdb9ecdea53c034395
Ying-Li Shih <norelle@Nor.local>
2020-03-08 00:39:29 +0800

    created selectors for collection and collection preview components and updated the components with data flow

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,16 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+//Object.keys() will take all of the keys of an object that we pass into it and gives it to us the keys in an array format 
+//After we get the keys, we map over that array of the keys, then get the value of our collections object at that key
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+//A curry function that takes the url parameter as an argument and returns another function(creatSelector())
+export const selectCollection = collectionUrlParam => createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+)
