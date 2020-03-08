eb51b84d7e4c028a9fad055bbd6fa142e00b3ea8
Ying-Li Shih <norelle@Nor.local>
2020-03-08 16:26:51 +0800

    fix typo

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,9 +7,9 @@ import rootReducer from './root-reducer';
 const middlewares = [];
 
 //Only when in the development mode, we show the action status in browser console
-if(process.env.NODE_ENV === 'development' {
+if(process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
-})
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
