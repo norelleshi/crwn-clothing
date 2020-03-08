7705f4b670ced9ec066afff565ec105aae9f6857
Ying-Li Shih <norelle@Nor.local>
2020-03-08 12:34:15 +0800

    fix the button style and tweet the height of cart dropdown a bit

--- src/components/stripe-button/stripe-button.component.js
+++ src/components/stripe-button/stripe-button.component.js
@@ -16,18 +16,20 @@ const StripeCheckoutButton = ({ price }) => {
 
     //StripeCheckout component takes a bunch of properties that enable different features that we have access to inside of our checkout dropdown
     return (
-        <StripeCheckout 
-            label='Pay Now'
-            name='CRWN Clothing Ltd.'
-            billingAddress
-            shippingAddress
-            image='https://sendeyo.com/up/d/f3eb2117da'
-            description={`Your total is $${price}`}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publishableKey}
-        />
+        <div className='pay-button'>
+            <StripeCheckout 
+                label='Pay Now'
+                name='CRWN Clothing Ltd.'
+                billingAddress
+                shippingAddress
+                image='https://sendeyo.com/up/d/f3eb2117da'
+                description={`Your total is $${price}`}
+                amount={priceForStripe}
+                panelLabel='Pay Now'
+                token={onToken}
+                stripeKey={publishableKey}
+            />
+        </div>
     )
 }
 
